838e2444d1811c102e40726fe08eeef80a3d206a
rmricci121 <rmricci121@gmail.com>
2020-09-26 10:44:09 -0400

    added seed file to test blog posts

--- popular_distractions_api/db/seeds.rb
+++ popular_distractions_api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Blog.create(author:'Robert Ricci', content:'This is a test post, to test that the posts are testing', date:'9/26/2020' )
