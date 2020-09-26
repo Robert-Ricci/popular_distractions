5b1e4b9b87f3fd0f1d4748d0fca5e1ae8afc3c6a
rmricci121 <rmricci121@gmail.com>
2020-09-26 10:37:23 -0400

    ran scaffold for blog model

---
+++ popular_distractions_api/db/migrate/20200926143600_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :blogs do |t|
+      t.string :author
+      t.string :content
+      t.string :date
+
+      t.timestamps
+    end
+  end
+end
