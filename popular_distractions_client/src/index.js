c1a6fa8e0aecb9eacdddc40bc62b1e1acd4a6c48
rmricci121 <rmricci121@gmail.com>
2020-09-26 11:20:24 -0400

    added code to index.js file, including imports for react, redux, thunk, and devtools

---
+++ popular_distractions_client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'react-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, 
+    composeWithDevTools(applyMiddleware{thunk}))
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById("root")
+    )
