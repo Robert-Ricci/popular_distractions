3769a674430695de35e8480cfe46568a87e0b87b
rmricci121 <rmricci121@gmail.com>
2020-09-27 06:27:37 -0400

    added blogcontainer

---
+++ popular_distractions_client/src/containers/BlogContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import BlogForm from '../componetns/blogForm';
+
+class blogContainer extends Component {
+    render() {
+        return (
+            <div>
+                <BlogForm/>
+            </div>
+        );
+    }
+}
+
+export default blogContainer;
