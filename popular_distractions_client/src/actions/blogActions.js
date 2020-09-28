e15479955f684d766f746400f3751fd5cd825e09
rmricci121 <rmricci121@gmail.com>
2020-09-28 05:26:21 -0400

    added ADD_BLOG action to blogActiosn file

--- popular_distractions_client/src/actions/blogActions.js
+++ popular_distractions_client/src/actions/blogActions.js
@@ -5,3 +5,15 @@ export const fetchBlogs = () => {
         .then(blogs => dispatch({ type: 'FETCH_BLOGS', payload: blogs}))
     }
 }
+
+export const addBlogs = (blog) =>{
+    return dispatch =>{
+        fetch('127.0.0.1:3000/blogs', {
+            method:'POST',
+            body: JSON.stringify(blog),
+            headers:{'Content-Type': 'application/json'}
+        })
+        .then(resp => resp.json)
+        .then(blog => dispatch({type: 'ADD_BLOG', payload:blog}))
+    }
+}
