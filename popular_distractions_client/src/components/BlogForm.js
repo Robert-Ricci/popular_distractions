0d1825599f50a337fd8cd9ec23445d2d3631ef0f
rmricci121 <rmricci121@gmail.com>
2020-09-28 05:18:10 -0400

    added state and form submit to forms componenet

--- popular_distractions_client/src/components/BlogForm.js
+++ popular_distractions_client/src/components/BlogForm.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class BlogForm extends Component {
+   
+     state = {
+        author: '',
+        title: '',
+        content: '',
+        date: ''
+    }
+
+   
+   
+
     render() {
         return (
-            <div>
-                <form>
+            
+                <form onSubmit={this.handleSubmit}>
                     <label>Name:</label>
                     <input type='text' value={this.props.name} onChange={this.handleChange} name='name'></input>
                     <br />
@@ -12,12 +23,12 @@ class BlogForm extends Component {
                     <label>Titlel:</label>
                     <input type='text' value={this.props.title} onChange={this.handleChange} name='title'></input>
                     <br />
-                    
+
                     <label>Content:</label>
                     <textarea  type='text' value={this.props.content} onChange={this.handleChange} name='content'></textarea>
 
                 </form>
-            </div>
+            
         );
     }
 }
