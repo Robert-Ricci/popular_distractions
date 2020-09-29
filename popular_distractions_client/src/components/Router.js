9796fec39fb334ec92a0e445667563d8dcadaa29
rmricci121 <rmricci121@gmail.com>
2020-09-29 15:54:39 -0400

    added home and blogs button for navbar, currently not working

--- popular_distractions_client/src/components/Router.js
+++ popular_distractions_client/src/components/Router.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 
 import BlogList from './BlogList';
 import Home from './Home';
 import BlogForm from './BlogForm';
+import NavBar from './NavBar';
 
 
 const Router = () => {
     return (
         
             <Switch>
+                <NavBar/>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/blogs' component={BlogList} />
                 <Route exact path='/blogform' component={BlogForm} />
