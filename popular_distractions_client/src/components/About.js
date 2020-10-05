863977d947bee803dd47c5c52fd22c7a5ba5e389
rmricci121 <rmricci121@gmail.com>
2020-10-05 14:24:12 -0400

    added css for the navbar

--- popular_distractions_client/src/components/About.js
+++ popular_distractions_client/src/components/About.js
@@ -3,18 +3,19 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
+        <div className="about">
             <br />
+            <h1>About</h1>
              <p>   I'm baby retro iPhone microdosing, ennui waistcoat raw denim messenger bag air plant twee squid cliche prism salvia occupy. 
                 Succulents glossier kitsch master cleanse. Mlkshk before they sold out tacos thundercats irony 90's artisan, enamel pin tousled shoreditch.
-                Cardigan affogato occupy, scenester migas biodiesel portland photo booth trust fund tbh mumblecore.
+                Cardigan affogato occupy, scenester migas biodiesel portland photo booth trust fund tbh mumblecore.</p>
 
-            Readymade skateboard +1, lomo wolf fixie salvia fingerstache hot chicken pinterest. 
+           <p> Readymade skateboard +1, lomo wolf fixie salvia fingerstache hot chicken pinterest. 
             Paleo enamel pin hell of vexillologist, chartreuse salvia drinking vinegar cred readymade.
             Ramps hexagon tousled 90's hoodie before they sold out air plant vinyl affogato vaporware. Mixtape retro humblebrag pug, sustainable DIY deep v banjo normcore distillery.
-             Microdosing yuccie chia offal tattooed enamel pin. Subway tile seitan cray, austin street art green juice organic thundercats fam lo-fi etsy.
+             Microdosing yuccie chia offal tattooed enamel pin. Subway tile seitan cray, austin street art green juice organic thundercats fam lo-fi etsy.</p>
 
-            Art party post-ironic bushwick distillery vice. 
+           <p> Art party post-ironic bushwick distillery vice. 
             Asymmetrical affogato gochujang franzen. +1 hexagon sustainable raw denim, twee letterpress DIY yuccie. 
             Gluten-free schlitz try-hard echo park chambray 90's austin normcore master cleanse shoreditch.
 
--- popular_distractions_client/src/components/NavBar.js
+++ popular_distractions_client/src/components/NavBar.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { NavLink }from 'react-router-dom';
 
 const link = {
-    width: '100px',
+    width: '310px',
     padding: '12px',
     margin: '0 6px 6px',
     background: 'blue',
     textDecoration: 'none',
     color: 'white',
+    
     
   }
 
