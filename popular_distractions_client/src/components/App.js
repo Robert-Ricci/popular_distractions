7cf7c06552dcc070ef2e1eb82434081a3ad4bfd6
Robert Ricci <rmricci121@gmail.com>
2020-10-06 15:47:05 -0400

    adjust margins for form, about and home

--- popular_distractions_client/src/components/App.js
+++ popular_distractions_client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import BlogContainer from './BlogContainer';
 import Router from './Router';
-import NavBar from './NavBar';
+
 // import { library } from '@fortawesome/fontawesome-svg-core'
 // import { faHome } from '@fortawesome/free-solid-svg-icons'
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
@@ -11,28 +11,26 @@ import './style.scss';
 const App = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">POPULAR DISTRACTIONS</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/About">About</Nav.Link>
-      <Nav.Link href="/blogs">Blog Posts</Nav.Link>
-      <Nav.Link href="/blogform">Blog Form</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-    </Nav>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+                <Navbar.Brand href="#home">POPULAR DISTRACTIONS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/About">About</Nav.Link>
+                        <Nav.Link href="/blogs">Blog Posts</Nav.Link>
+                        <Nav.Link href="/blogform">Blog Form</Nav.Link>
+                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
   </Navbar.Collapse>
 </Navbar>
-            <NavBar />
+            
             <Router/>
             <BlogContainer />
            
