e32d63d679754439105f6dd248c6a6f5dd7438a5
Robert Ricci <rmricci121@gmail.com>
2020-10-06 20:26:41 -0400

    rounded home component image with react-bootstrap

--- popular_distractions_client/src/components/Home.js
+++ popular_distractions_client/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Distracted from '../distracted.jpg'
+import Image from 'react-bootstrap/Image'
+
 const Home = () => {
     return (
         <div className="home">
             <br />
             <h1> Welcome to Popular Distractions!</h1>
             <h2>The premier place to get distracted</h2>
-            <img src ={Distracted} alt="distracted" />
+            <Image src ={Distracted} alt="distracted" roundedCircle/>
         </div>
     );
 };
