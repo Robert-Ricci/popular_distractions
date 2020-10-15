2c9c6ed3b84b68448925a4c15a3139c9a6cbf2a1
Robert Ricci <rmricci121@gmail.com>
2020-10-15 13:17:07 -0400

    This commit creates a link for email to open email client

--- popular_distractions_client/src/components/Contact.js
+++ popular_distractions_client/src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="contact">
-            <p>If you like to get in touch email me at rmricci121@gmail.com</p>
+            <p>If you like to get in touch email me at <a href="mailto:rmricci121@gmail.com?subject='Sup" >rmricci121@gmail.com</a> </p>
         </div>
     );
 };
